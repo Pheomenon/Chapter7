985a02c18d0178374c60c435c7c86e32d83403b1
Your Name <1149650442@qq.com>
2019-02-23 17:15:54 +0800

    c2

--- Chapter7/Chapter7.cpp
+++ Chapter7/Chapter7.cpp
@@ -3,12 +3,31 @@
 
 #include "pch.h"
 #include <iostream>
+double average(double x, double y);
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	cout << "Enter two nums: ";
+	double num1, num2;
+	while (cin >> num1 && cin >> num2) {
+		if (num1 == 0 || num2 == 0) {
+			return 0;
+	}
+		else {
+			cout << "AVERAGE: ";
+			cout << average(num1, num2)<< endl;
+			cout << "Enter two nums: ";
+		}
+	}
 }
 
+double average(double x, double y) {
+	double avg;
+	avg = 2.0*x*y / (x + y);
+	return avg;
+}
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
 // 调试程序: F5 或调试 >“开始调试”菜单
 
@@ -19,3 +38,5 @@ int main()
 //   4. 使用错误列表窗口查看错误
 //   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
 //   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+
+
---
+++ c2/c2.cpp
@@ -0,0 +1,60 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <math.h>
+
+void SaveGolf(int *golf);
+void ShowGolf(const int *golf);
+void AverageGolf(const int *golf);using namespace std;
+
+int const maxscore = 10;
+int TrueSize = 0;
+
+int main()
+{
+	cout << "Input the score of golf!" << endl;
+	int golf[maxscore];
+	SaveGolf(golf);
+	ShowGolf(golf);
+	AverageGolf(golf);
+	return 0;
+}
+
+void SaveGolf(int *golf)
+{
+	cout << "If you want to end the input, press the letter!" << endl;
+	for (int i = 0; i < maxscore; i++)
+	{
+		cout << "Input the " << i + 1 << " Score!" << endl;
+		if (cin >> golf[i])
+		{
+		}
+		else
+		{
+			TrueSize = i;
+			break;
+		}
+	}
+	cout << "Enter end!" << endl;
+}
+
+void ShowGolf(const int * golf)
+{
+	cout << "Show the golf socre!" << endl;
+
+	for (int i = 0; i < TrueSize; i++)
+	{
+		cout << golf[i] << '\t';
+	}
+	cout << endl;
+}
+
+void AverageGolf(const int *golf)
+{
+	int sum = 0;
+	for (int i = 0; i < TrueSize; i++)
+	{
+		sum += golf[i];
+	}
+	cout << "The score average is : " << sum / TrueSize << "!" << endl;
+}
+
